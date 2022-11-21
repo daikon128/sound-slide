ac30f7ad99c0bc6c2aac3b4ebdbbbc5a57c11abe
daikon128 <ndaichi06@gmail.com>
2022-11-22 00:27:36 +0900

    build app as library

--- slide/vite.config.ts
+++ slide/vite.config.ts
@@ -5,10 +5,19 @@ export default defineConfig({
   server: {
     port: 4000
   },
+  preview: {
+    port: 14000
+  },
   plugins: [
     viteReact()
   ],
   build: {
-    outDir: "build"
-  }
+    outDir: "build",
+    lib: {
+      entry: 'src/index.tsx',
+      name: 'slide',
+      formats: ['iife'],
+      fileName: (_format) => `slide.js`
+    }
+  },
 })
--- sound/vite.config.js
+++ sound/vite.config.js
@@ -4,8 +4,17 @@ export default {
   server: {
     port: 3000
   },
+  preview: {
+    port: 13000
+  },
   plugins: [react()],
   build: {
-    outDir: "build"
+    outDir: "dist",
+    lib: {
+      entry: 'src/index.jsx',
+      name: 'sound',
+      formats: ['iife'],
+      fileName: (_format) => `sound.js`
+    }
   }
 }
