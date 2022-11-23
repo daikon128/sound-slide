98d38c4a2ad65e10979502ccade385ac5e862086
daikon128 <ndaichi06@gmail.com>
2022-11-23 12:46:28 +0900

    create front

---
+++ front/pages/index.tsx
@@ -0,0 +1,13 @@
+import styles from '../styles/Home.module.css'
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>sound-slide</title>
+      </Head>
+      hello
+    </div>
+  )
+}
