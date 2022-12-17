220f38ccb6667451ba48a3fc89cddcf2c209bc29
daikon128 <ndaichi06@gmail.com>
2022-12-17 23:05:32 +0900

    loading実装中

--- front/components/audio/UploadAudio.tsx
+++ front/components/audio/UploadAudio.tsx
@@ -1,19 +1,24 @@
 import React from "react";
 import axios from "axios";
-import {Button} from "@mui/material";
+import {Button, CircularProgress} from "@mui/material";
 import {Upload} from "@mui/icons-material";
 
 export const UploadAudio = () => {
   const inputFileElement = React.createRef<HTMLInputElement>()
+  const [isLoading, setIsLoading] = React.useState(false)
+
   const upload = async () => {
+    setIsLoading(true)
+    await new Promise(_ => setTimeout(_, 1000))
     const response = await axios.post('/api/audio', {
       foo: "bar"
-    })
+    }).finally(() => setIsLoading(false))
     console.log(response)
   }
 
   return (
     <div>
+      { isLoading ? <CircularProgress /> : null}
       <div>
         音声ファイルをアップロードしてください
       </div>
